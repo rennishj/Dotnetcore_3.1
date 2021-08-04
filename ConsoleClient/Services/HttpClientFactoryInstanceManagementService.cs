ed37b550158e66fb9d7421e8a9f47f35fecba847
Rennish Joseph <rennish.joseph@bkfs.com>
2021-08-04 13:01:48 -0400

    Adding httpclient test

--- ConsoleClient/Services/HttpClientFactoryInstanceManagementService.cs
+++ ConsoleClient/Services/HttpClientFactoryInstanceManagementService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,13 +16,19 @@ namespace ConsoleClient.Services
 
         private async Task TestDisposeHttpClient(CancellationToken cancellationToken)
         {
-            string url = "https://google.com";
+            string url = "https://google.com";          
 
             for (int i = 0; i < 10; i++)
             {
-                using (var httpClient = new HttpClient())
+                //only if you use proxy   - if inside a corporate network
+                var clientHandler = new HttpClientHandler();
+                var proxy = WebRequest.GetSystemWebProxy();
+                proxy.Credentials = CredentialCache.DefaultCredentials;
+                clientHandler.Proxy = proxy;
+                using (var httpClient = new HttpClient(clientHandler))
                 {
-                    var request = new HttpRequestMessage(HttpMethod.Get, url);
+                    
+                    var request = new HttpRequestMessage(HttpMethod.Get, url);                 
 
                     using (var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken))
                     {
